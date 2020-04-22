7190d8ad13393c4763ce895abd9df985b1261519
isabel k. lee <hello@isabelklee.co>
2020-04-21 20:42:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Your code here
+// move dodger left
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 10}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+        console.log("moved left")
+    }
+});
+
+// move dodger right
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "")
+    let left = parseInt(leftNumbers, 10)
+
+    if (left < 360) {
+        dodger.style.left = `${left + 10}px`
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+        console.log("moved right")
+    }
+})
